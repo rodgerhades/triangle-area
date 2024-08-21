4c1c1439ea7cbe9ee212904fb3cf33eb59285525
Rodger Cabrerisso <83619204+rodgerhades@users.noreply.github.com>
2024-08-21 14:02:28 -0300

    Add files via upload

---
+++ areaTriangulo.py
@@ -0,0 +1,10 @@
+#base * altura /2
+
+#base
+base = int(input("coloque a base "))
+altura = int(input("coloque a altura "))
+
+#multiplicar
+multi = base * altura
+resultado = multi / 2
+print("a area do triangulo é:",resultado)
